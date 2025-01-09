7dab946c6e4cd5ed02e748c47ab765d0ee2039ef
ahsanshaikh27 <shaikhahsan8580@gmail.com>
2025-01-09 23:02:37 +0530

    New Added script1.sh

--- script1.sh
+++ script1.sh
@@ -1,8 +1,9 @@
-echo "Hello Faizan!"
-name="Ahmad"
-echo $name
-echo "Enter You Name:"
-read Name
-echo "Hello, $name!"
-echo "Enter You Age"
+echo "Enter Your Name"
+read name
+echo "Enter Your Age:"
 read number
+echo "Enter Your Mobile Number"
+read number
+echo "Enter Your EmailId"
+read name
+
